72cd730c9e89b73869225a8391dbd4c360497a9c
digized <Zuraiz.zafar97@gmail.com>
2017-06-07 11:46:38 -0400

    another random message

--- app/src/main/java/zzafa015/uottawa/ca/tipcalculator/MainActivity.java
+++ app/src/main/java/zzafa015/uottawa/ca/tipcalculator/MainActivity.java
@@ -1,5 +1,8 @@
 package zzafa015.uottawa.ca.tipcalculator;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.Preference;
@@ -9,17 +12,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+import android.util.Log;
+import android.view.*;
 import android.widget.*;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView labelCurrency, labelSummary;
+    TextView labelCurrency;
     EditText editTip, editAmount, editPerson;
+    RatingBar rb;
+    View dialogView;
+    Button addBtn,removeBtn, tipSuggestBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,50 +32,35 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         labelCurrency = (TextView) findViewById(R.id.lbl_currency);
         labelCurrency.setText(preferences.getString("currency_preference","$"));
-        final MainActivity mainActivity = this;
         editTip =(EditText) findViewById(R.id.edit_tip);
         editTip.setHint(preferences.getString("tip_preference", "13.7f"));
-//        editTip.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                mainActivity.onTextChange();
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
 
         labelCurrency.setText(preferences.getString("currency_preference","$"));
 
-        labelSummary = (TextView) findViewById(R.id.lbl_Sum);
-
         editAmount = (EditText) findViewById(R.id.edit_amount);
-//        editAmount.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                mainActivity.onTextChange();
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
 
         editPerson = (EditText) findViewById(R.id.edit_persons);
+
+        addBtn = (Button) findViewById(R.id.btn_add_person) ;
+        removeBtn = (Button) findViewById(R.id.btn_subtract_person);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num = Integer.parseInt(editPerson.getText().toString());
+                editPerson.setText(String.valueOf(num+1));
+            }
+        });
+        removeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num = Integer.parseInt(editPerson.getText().toString());
+                if(num - 1 > 0)
+                editPerson.setText(String.valueOf(num - 1));
+            }
+        });
+
+        tipSuggestBtn = (Button) findViewById(R.id.btn_suggestTip);
+
 
     }
 
@@ -97,24 +86,61 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-//    public void onTextChange(){
-//        float amount, tip;
-//        int persons;
-//
-//        persons = editPerson.getText().toString().equals("") ? Integer.parseInt(editPerson.getHint().toString()) : Integer.parseInt(editPerson.getText().toString());
-//
-//        amount = Float.parseFloat(editAmount.getText().toString());
-//        tip = editTip.getText().toString().equals("") ? Float.parseFloat(editTip.getHint().toString()) : Float.parseFloat(editTip.getText().toString());
-//
-//        labelSummary.setText(String.valueOf(amount+(amount*tip/100)));
-//
-//    }
-
     public void onSummaryClick(View view){
-        Intent intent = new Intent(this, SummaryActivity.class);
-        intent.putExtra("amount",Float.parseFloat(editAmount.getText().toString()));
-        intent.putExtra("tip",editTip.getText().toString().equals("") ? Float.parseFloat(editTip.getHint().toString()) : Float.parseFloat(editTip.getText().toString()));
-        intent.putExtra("persons",editPerson.getText().toString().equals("") ? Integer.parseInt(editPerson.getHint().toString()) : Integer.parseInt(editPerson.getText().toString()));
-        startActivity(intent);
+        if(editAmount.getText().toString().equals("") || Integer.parseInt(editPerson.getText().toString())<1){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Please ensure correct information is entered")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .show();
+
+        }else {
+            Intent intent = new Intent(this, SummaryActivity.class);
+            intent.putExtra("amount", Float.parseFloat(editAmount.getText().toString()));
+            intent.putExtra("tip", editTip.getText().toString().equals("") ? Float.parseFloat(editTip.getHint().toString()) : Float.parseFloat(editTip.getText().toString()));
+            intent.putExtra("persons", editPerson.getText().toString().equals("") ? Integer.parseInt(editPerson.getHint().toString()) : Integer.parseInt(editPerson.getText().toString()));
+            startActivity(intent);
+        }
+    }
+
+    public void onRateSuggestionClick(View view){
+
+        LayoutInflater layoutInflater = this.getLayoutInflater();
+        dialogView = layoutInflater.inflate(R.layout.dialog_xml,null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        rb = (RatingBar) dialogView.findViewById(R.id.rate_foodRate);
+        final TextView lblRating = (TextView) dialogView.findViewById(R.id.lbl_tipsuggestval);
+        lblRating.setText("Tip: "+String.valueOf(10+rb.getRating()*2)+"%");
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                lblRating.setText("Tip: "+String.valueOf(10+v*2) + "%");
+
+
+            }
+        });
+        builder.setTitle(R.string.suggest_tip)
+                .setView(dialogView)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        float val = 10+rb.getRating()*2;
+                        editTip.setText(String.valueOf(val));
+
+
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .show();
+
     }
 }
--- app/src/main/java/zzafa015/uottawa/ca/tipcalculator/SummaryActivity.java
+++ app/src/main/java/zzafa015/uottawa/ca/tipcalculator/SummaryActivity.java
@@ -5,7 +5,14 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -13,6 +20,11 @@ public class SummaryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        //getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        NumberFormat numberFormat = new DecimalFormat("0.00");
         Intent intent = getIntent();
         int person;
         float amount, tip;
@@ -21,7 +33,7 @@ public class SummaryActivity extends AppCompatActivity {
 
         person = intent.getIntExtra("persons",1);
         amount = intent.getFloatExtra("amount", 0);
-        tip = intent.getFloatExtra("name", 13.5f);
+        tip = intent.getFloatExtra("tip", 13.5f)/100 * amount;
         String currency = preferences.getString("currency_preference","$");
 
         double amountPerson = amount/person;
@@ -29,22 +41,31 @@ public class SummaryActivity extends AppCompatActivity {
 
         StringBuilder builder = new StringBuilder();
 
-        builder.append("Total Amount:\t\t\t");
-        builder.append(currency+amount + "\n\n");
-        builder.append("Tip Amount:\t\t\t");
-        builder.append(currency+tip + "\n\n");
-        builder.append("Person(s) paying:\t\t\t");
-        builder.append(currency+person + "\n\n");
-        builder.append("Amount per person:\t\t\t");
-        builder.append(currency+amountPerson + "\n\n");
-        builder.append("Tip per person:\t\t\t");
-        builder.append(currency+tipPerson + "\n\n");
-        builder.append("Total per person:\t\t\t");
-        builder.append(currency+(amountPerson+tipPerson));
+        builder.append("Total Amount:\n");
+        builder.append(currency+numberFormat.format(amount)+ "\n\n");
+        builder.append("Tip Amount:\n");
+        builder.append(currency+numberFormat.format(tip)+ "\n\n");
+        builder.append("Person(s) paying:\n");
+        builder.append(person + "\n\n");
+        builder.append("Amount per person:\n");
+        builder.append(currency+numberFormat.format(amountPerson )+ "\n\n");
+        builder.append("Tip per person:\n");
+        builder.append(currency+numberFormat.format(tipPerson) + "\n\n");
+        builder.append("Total per person:\n");
+        builder.append(currency+numberFormat.format(amountPerson+tipPerson));
 
         TextView summary = (TextView)findViewById(R.id.lbl_summary_content);
 
         summary.setText(builder.toString());
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
